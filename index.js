1ea304888ac9c10e5503c941b917e1db524da4b7
gpsushmitha <sushmithagp07@gmail.com>
2024-09-17 16:14:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+const display = document.getElementById('display');
+
+function appendChar(char) {
+    display.value  += char;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value) ;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+function calculateSqrt() {
+    try {
+        display.value = Math.sqrt(eval(display.value));
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function calculateTrig(func) {
+    try {
+        let value = eval( display.value);
+        if (func === 'sin') {
+            display.value = Math.sin(value);
+        } else if (func === 'cos') {
+            display.value = Math.cos(value);
+        } else if (func === 'tan') {
+            display.value = Math.tan(value);
+        }  
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function calculatePower() {
+    try {
+        display.value = Math.pow(eval( display.value), 2);
+   
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function calculateExp() {
+    try {
+        display.value = Math.exp(eval(display.value));
+     
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function calculateLog() {
+    try {
+        display.value = Math.log10(eval( display.value));
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function calculateLn() {
+    try {
+        display.value = Math.log( eval(display.value));
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function calculatePi() {
+    try {
+        display.value = Math.PI( eval(display.value));
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+
